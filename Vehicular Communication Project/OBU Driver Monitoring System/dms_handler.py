b575d4251000560d2576942beeb22f722f03a44b
kanopo <dmitri.ollari@protonmail.com>
2024-01-19 11:56:50 +0100

    Revert "deleted ald files"
    This reverts commit e4d0d4eb6b21512945ae85940aa512a65e785b84.

---
+++ Vehicular Communication Project/OBU Driver Monitoring System/dms_handler.py
@@ -0,0 +1,109 @@
+import paho.mqtt.client as mqtt
+from aiohttp import web
+from random import randint
+import time
+import threading 
+import json
+
+class MqttClient:
+    def __init__(self, host, port, id):
+        self.client = mqtt.Client(client_id = id)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        self.host = host
+        self.port = port
+
+        self.mqtt_connection()
+
+    def mqtt_connection(self):
+        self.client.connect(self.host, self.port, 60)
+
+    def on_connect(self, client, userdata, flags, rc):
+        print('Connected with result code ' + str(rc))
+    
+    def on_message(self, client, userdata, msg):
+        return 0
+
+    def subscribe(self, topic):
+        self.client.subscribe(topic)
+        print('Subscribed to ' + topic)
+    
+    def publish(self,topic, message):
+        self.client.publish(topic, message)
+
+    def start(self):
+        self.client.loop_start()
+
+    def stop(self):
+        self.client.loop_stop()
+
+class HttpServer:
+    def __init__(self, dms_handler, port):
+        self.app = web.Application()
+        self.routes = web.RouteTableDef()
+        self.port = port
+
+        self.dms_handler = dms_handler
+
+        @self.routes.get('/dms')
+        async def get_handler(request):
+            status = self.dms_handler.display_status()
+            return web.Response(text = status)
+
+        self.app.add_routes(self.routes)
+    
+    def start(self):
+        web.run_app(self.app, port = self.port)
+    
+    def stop(self):
+        web.run_app(self.app)
+
+class DmsHandler:
+    def __init__(self, mqtt_client):
+        self._mqtt_client = mqtt_client
+        self._current_status = 'normal'
+
+    def change_status(self):
+        status = ['Angry', 'Tired', 'Drunk', 'Distracted', 'Normal']
+        n = randint(0, 4)
+        new_status = status[n]
+        self._current_status = new_status
+        self._mqtt_client.publish("vc2324/dms", new_status)
+        print(new_status)
+    
+    def display_status(self):
+        return self._current_status
+
+#CONFIGURATION
+with open('Config.json', 'r') as file:
+	config_file = json.load(file)
+
+for o in config_file['Obu']:
+    if o['name'] == 'Driver Monitoring System':
+        http_server_port = o['http_port']
+        client_id = o['mqtt_id']
+
+for b in config_file['MqttBroker']:
+    broker_ip = b['ip']
+    broker_port = b['port']
+
+mqtt_client = MqttClient(broker_ip, broker_port, client_id)
+dms_handler = DmsHandler(mqtt_client)
+http_server = HttpServer(dms_handler, http_server_port)
+
+mqtt_client.subscribe("vc2324/dms")
+mqtt_client.start()
+
+# Start the thread for changing the status
+def change_status_thread():
+    while True:
+        dms_handler.change_status()
+        time.sleep(10)
+
+change_status_thread = threading.Thread(target=change_status_thread)
+change_status_thread.start()
+
+http_server.start()
+
+mqtt_client.stop()
---
+++ Vehicular Communication Project/OBU Weather Information/WeatherHandler.py
@@ -0,0 +1,134 @@
+import python_weather
+import json
+import asyncio
+import os
+import paho.mqtt.client as mqtt
+from aiohttp import web
+
+class MqttClient:
+    def __init__(self, host, port, id):
+        self.client = mqtt.Client(client_id = id)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        self.host = host
+        self.port = port
+
+        self.mqtt_connection()
+
+    def mqtt_connection(self):
+        self.client.connect(self.host, self.port, 60)
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+    
+    def on_message(self, client, userdata, msg):
+        print(msg.topic + " " + str(msg.payload))
+
+    def subscribe(self, topic):
+        self.client.subscribe(topic)
+        print("Subscribed to " + topic)
+    
+    def publish(self,topic, message):
+        self.client.publish(topic, message)
+
+    def start(self):
+        self.client.loop_start()
+
+    def stop(self):
+        self.client.loop_stop()
+
+class HttpServer:
+    def __init__(self, mqtt_client, port):
+        self.app = web.Application()
+        self.routes = web.RouteTableDef()
+        self.port = port
+        self.mqtt_client = mqtt_client
+
+        @self.routes.post('/weathercurrent')
+        async def post_handler(request):
+
+            data_curr = await request.post()
+
+            current_weather = {}
+
+            current_weather['city'] = str(data_curr['current_city'])
+            current_weather['temperature'] = str(data_curr['current_temperature'])
+            current_weather['weather'] = str(data_curr['current_kind'])
+
+            broad_msg = json.dumps(current_weather)
+
+            self.mqtt_client.publish("vc2324/weather-current", broad_msg)
+
+            #print("POST (current weather) ricevuto")
+            #print("v----v")
+            #print(data_curr)
+            #print(data_curr['current_description'])
+            #print(data_curr['current_kind'])
+            #print("------")
+
+            return web.Response(text = "ok")
+
+        @self.routes.post('/weatherforecast')
+        async def post_handler1(request):
+            
+            data = await request.json()
+            data_forec = json.loads(data)
+
+            forecast_weather = {}
+
+            for i in data_forec:
+                row = {}
+                row['time'] = str(data_forec[i]['time'])
+                row['temperature'] = str(data_forec[i]['temperature'])
+                row['weather'] = str(data_forec[i]['kind'])
+                forecast_weather[i] = row
+
+            broad_msg = json.dumps(forecast_weather)
+
+            self.mqtt_client.publish("vc2324/weather-forecast", broad_msg)
+            
+            #data = json.loads(data_forec)
+            #print("POST (forecast) ricevuto")
+            #print("v----v")
+            #print(data_forec)
+            #print("------")
+            #print(data['0']['kind'])
+            #print("------")
+            
+            return web.Response(text = "ok")
+
+        self.app.add_routes(self.routes)
+
+
+    def start(self):
+        web.run_app(self.app, port = self.port)
+    
+    def stop(self):
+        web.run_app(self.app)
+
+#CONFIGURATION
+with open('Config.json', 'r') as file:
+	config_file = json.load(file)
+
+for o in config_file['Obu']:
+    if o['name'] == 'Weather Information':
+        http_server_port = o['http_port']
+        client_id = o['mqtt_id']
+
+for b in config_file['MqttBroker']:
+    broker_ip = b['ip']
+    broker_port = b['port']
+        
+mqtt_client = MqttClient(broker_ip, broker_port, client_id)
+        
+http_server = HttpServer(mqtt_client, http_server_port)
+
+mqtt_client.subscribe("vc2324/weather-current")
+mqtt_client.subscribe("vc2324/weather-forecast")
+mqtt_client.start()
+http_server.start()
+
+mqtt_client.stop()
+
+print("Program terminated!")
