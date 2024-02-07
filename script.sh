0213a4684adc56faa0e7ff826af33be673bfc8d1
kanopo <dmitri.ollari@protonmail.com>
2024-02-07 22:51:45 +0100

    project working

--- script.sh
+++ script.sh
@@ -22,24 +22,24 @@ trap cleanup INT
 # Check if the virtual environment exists
 if [ ! -d "$VENV_PATH" ]; then
     # Create virtual environment
-   python3 -m venv "$VENV_PATH"
+   python3.10 -m venv "$VENV_PATH"
 fi
 
 # Activate the virtual environment
 source "$VENV_PATH/bin/activate"
 
 # Install requirements
-pip install -r "$REQUIREMENTS_PATH"
+pip3.11 install -r "$REQUIREMENTS_PATH"
 
 # Start Python scripts in the background
-python3.10 "./OBUCentralUnit/central_obu_handler.py" &
-python3.10 "./OBUWeatherInformation/WeatherHandler.py" &
-python3.10 "./OBUWeatherInformation/ClientHTTP.py" &
-python3.10 "./OBUKeylessSystem/KeylessHandeler.py" &
-python3.10 "./OBUBrakingSystem/braking_handler.py" &
-python3.10 "./OBUDriverMonitoringSystem/dms_handler.py" &
-python3.10 "./OBUInfotainmentSystem/RadioHandler.py" &
-python3.10 "./Gui/ControlGui.py" &
+python3.11 "./OBUCentralUnit/central_obu_handler.py" &
+python3.11 "./OBUWeatherInformation/WeatherHandler.py" &
+python3.11 "./OBUWeatherInformation/ClientHTTP.py" &
+python3.11 "./OBUKeylessSystem/KeylessHandeler.py" &
+python3.11 "./OBUBrakingSystem/braking_handler.py" &
+python3.11 "./OBUDriverMonitoringSystem/dms_handler.py" &
+python3.11 "./OBUInfotainmentSystem/RadioHandler.py" &
+python3.11 "./Gui/ControlGui.py" &
 
 # Wait for all background jobs to finish
 wait
